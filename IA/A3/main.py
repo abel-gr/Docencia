c3dac5997561251b57b7b61f87431dd19988578f
Abel <abel.garciar@e-campus.uab.cat>
2024-02-01 19:15:56 +0100

    Update main.py

--- IA/A3/main.py
+++ IA/A3/main.py
@@ -17,30 +17,32 @@ from pyscript import display
 
 def learning_rate_button(event):
     
-    learning_rate = document.querySelector("#learning_rate_text")
+    learning_rate = float(document.querySelector("#learning_rate_text"))
     
-    dataset = load_breast_cancer()
-
+    # dataset = load_breast_cancer()
+    dataset = load_digits()
+    
+    
     x = dataset.data
     y = dataset.target
-
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-    model = MLPClassifier(learning_rate_init=learning_rate, random_state=42)
+    
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=42)
+    
+    model = MLPClassifier(learning_rate_init=learning_rate, hidden_layer_sizes=(4), random_state=42)
     model.fit(X_train, y_train)
-
+    
     y_pred = model.predict(X_test)
-
+    
     accuracy = accuracy_score(y_test, y_pred)
     cm = confusion_matrix(y_test, y_pred)
-
-
+    
+    
     fig, ax = plt.subplots(1, 1, figsize=(5,5), dpi=350)
     ax.set_title("Confusion matrix\n with learning\n rate = " + str(learning_rate))
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=dataset.target_names)
     disp.plot(ax=ax, cmap="Blues", values_format="d")
     plt.show()
-    
+        
     display(fig, target="output_confusion_matrix")
     
-    document.querySelector("#accuracy").innerText = accuracy
+    document.querySelector("#accuracy").innerText = round(accuracy, 6)
